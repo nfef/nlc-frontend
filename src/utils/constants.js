55d5f3002cf115f11ebf58194ce67627c620a979
ngoumou foe <foefabrice.pro@gmail.com>
2024-12-17 05:10:25 +0100

    change: modification du baseUrl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-    baseUrl: 'https://backendfestival.newlevelcorporation.org/backendfestival/',
+    baseUrl: 'https://api.newlevelcorporation.org/api/',
     publicKey: 'PK_4kYj2PUzybaQYXaMUdYC',
     unique_vote_amount: 100,
     unique_vote_amount_foreign: 6
